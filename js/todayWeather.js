ff6b379baa1996adbf2c804a6236c879d9c76880
Bogdan <42995409+BogdanMaximovic@users.noreply.github.com>
2026-01-12 13:14:23 +0100

    Update API key for weather data retrieval

--- js/todayWeather.js
+++ js/todayWeather.js
@@ -15,7 +15,7 @@ document.getElementById('search').addEventListener('click', function(){
 	const city = document.getElementById('text').value
 	if (!city) alert('Please insert city name!');
 	const apiSite = 'https://api.openweathermap.org/data/2.5/weather?q=' + city + '&';
-    const apiKey = 'appid=d108ea747e0359604e7c0b4e531b4f6c';
+    const apiKey = 'appid=a4c35d01517f9258aa465d9e2f5baff8';
     const url = apiSite + apiKey;
 
 	function getWeather(data) {
